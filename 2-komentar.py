51289d42e5d0f8b29b77cc8f46a91352d00473dd
Ranga-Prt1 <ranggapratama030319@gmail.com>
2025-06-25 01:09:29 +0800

    menambahkan komentar

---
+++ 2-komentar.py
@@ -0,0 +1,11 @@
+
+print("gunakan # lalu ketikkan sesuatu untuk menulis komentar")
+
+# ini adalah contoh komentar di python komentar tidak akan di tampilkan saat di jalankan
+
+"""
+ini juga contoh komentar pada python 
+untuk komentar yang lebih panjang dengan menggunakan tanda kutip tiga 
+fungsi komentar seperti kedua ini adalah untuk catatan bagi programmer
+"""
+
