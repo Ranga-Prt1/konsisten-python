35b3612719dbc085fc612e4d6afad50990887213
rangga <ranggapratama0819@gmail.com>
2025-06-25 00:22:47 +0800

    halo python

---
+++ 1-halo.py
@@ -0,0 +1,7 @@
+
+print("halo dunia"); 
+# jika ingin menampilkan sesuatu maka gunakan print 
+# lalu letakan dalam tanda kurung dan di dalam tanda kutip 
+
+print('halo indonesia')
+# dapat juga menggunakan petik satu untuk menampilkan sesuatu
