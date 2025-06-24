0ec9175036a96c86edde7d7b6d932b38a1f6e469
Ranga-Prt1 <ranggapratama030319@gmail.com>
2025-06-25 01:10:50 +0800

    membuat variabel

---
+++ 3-variabel.py
@@ -0,0 +1,25 @@
+""" selanjutnya adalah variabel, variabel dapat dikatakan sebagai wadah untuk menampung sebuah nilai"""
+
+kardus = "apel"      # variabel kardus berisi nilai apel
+print(kardus);       # perintah untuk menampilkan, membungkus variabel yang di buat
+                     # maka akan jika di jalankan yang tampil adalah isi variabel tersebut yaitu ape
+                     
+piring = "nasi"
+print(piring);       # variabel piring berisi nilai nasi
+                     # jika di jalankan maka yang tampil adalah nasi
+                     
+
+""" berikutnya adalah mengubah nilai sebuah variabel """
+
+gelas = "air putih"  # awalnya variabel gelas di isi dengan air putih
+gelas = "kopi"       # kemudian variabel gelas di tulis lagi sekarang dengan isi kopo
+print(gelas)         # jika di jalankan 
+                     # maka yang tampil adalah kopi karena  gelas yang awalnya air putih di ubah menjadi kopi
+                     
+
+lemari = "baju"
+lemari = "celana"
+lemari = "jaket"
+print(lemari)        # jika di jalankan maka yang tampil adalah jaket
+                     # karena nilai variabel lemari di ubah menjadi jaket, dan tidak ada perubahan lagi setelah itu
+                     
