4d5ba7257bcf0ae2a8966c1818ecd8a6eab76fe3
Ranga-Prt1 <ranggapratama030319@gmail.com>
2025-06-25 23:47:26 +0800

    tipe data part 1

---
+++ 4-tipe_data_1.py
@@ -0,0 +1,27 @@
+""" Tipe data di sebut juga dengan jenis nilai yang digunakan dalam progran"""
+
+# tipe data string (berupa huruf,angka dan simbol di dalam tanda kutip satu maupun dua)
+
+piring1 = "kue"
+kodeNama = "01-Nana"
+HargaBarang = "Rp. 10.000"
+
+# tipe data integer (berupa angka lebih tepatnya bilangan bulat)
+
+hargaNormal = 10000
+NomorKamar = 101
+
+# tipe data float (beruba bilangan pecaran dalam program ditulis dengan koma)
+
+hargaDiskon = 5000.00
+Pi = 3.14
+
+# tipe data boolean (berupa benar atau salah)
+
+Stock_Barang = True
+kuantitasGudang = False
+
+# tipe data None (tidak ada nilai)
+# digunakan untuk menyatakan bahwa suatu variabel belum di inisialisasi
+
+Wilayah_A = None
