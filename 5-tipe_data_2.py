e082507869e601ec9e03d7e1ad4276857e248eb4
Ranga-Prt1 <ranggapratama030319@gmail.com>
2025-06-27 00:50:45 +0700

    tipe data part 2

---
+++ 5-tipe_data_2.py
@@ -0,0 +1,15 @@
+# list adalah Kumpulan data berurutan, bisa diubah.
+buah = ['apel', 'jeruk']
+
+# tuple adalah Kumpulan data berurutan, tidak bisa diubah.
+angka = (1, 2, 3)
+
+# set adalah Kumpulan data acak (tidak berurutan), tidak boleh ada yang sama.
+unik = {98, 99, 100}
+
+# frozenset Sama kayak set yaitu kumpulan data acak (tidak berurutan), tapi tidak bisa diubah.
+fs = frozenset({9, 10, 11})
+
+# dict disebut juga (dictionary) yaitu Pasangan key: value
+orang = {'nama': 'Lia', 'umur': 20}
+hewan = {'nama': 'kucing', 'umur': 2}
