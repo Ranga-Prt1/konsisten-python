a7262f93428a8a4524856694ae694ccb2e64e056
Ranga-Prt1 <ranggapratama030319@gmail.com>
2025-06-30 00:15:14 +0700

    numbers in python

---
+++ 6-bilangan.py
@@ -0,0 +1,19 @@
+""" dalam python terdapat integer atau di sebut juga dengan bilangan bulat"""
+
+angkaGanjil = 121 
+angkaGenap = 100
+
+# ini adalah contoh integer disingkat int atau di sebut juga dengan bilangan bulat
+
+""" selanjutnya float atau di sebut juga dengan bilangan pecahan"""
+
+panjangMobil = 4.5
+tinggiMobil = 1.5
+
+# contoh float bisa di lihat dari penulisanya yang berkoma
+
+""" Cara melakukan cek tipe data sebuah program """
+# dengan menggunakan type()
+
+print(type(angkaGanjil))  # jika di jalankan maka yang tampil <class 'int'> int = integer / bilangan bulat
+print(type(panjangMobil)) # jika di jalankan maka yang tampil <class 'float'> float = bilangan pecahan
