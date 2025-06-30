855599b809f6a036e93c45af28232389cc086d78
Ranga-Prt1 <ranggapratama030319@gmail.com>
2025-07-01 00:18:12 +0700

    String Dasar Python

---
+++ 7-string.py
@@ -0,0 +1,23 @@
+""" Dalam Python tipe data string (berupa huruf, angka dan simbol di dalam tanda kutip satu maupun dua) """
+
+#contoh dalam bentuk kata dan kalimat
+
+NamaSiswa = "ALGAZALI";
+Nama_Guru = "Mr. Rizki";
+
+#contoh dalam bentuk angka
+
+TotalHPP = "Rp. 70.000";
+KamarHotel = "101";
+
+#contoh dalam bentuk simbol
+
+SimbolBesarDari = ">";
+SimbolKecilDari = "<";
+Simbol_TidakSama = "!=";
+
+#contoh dalam kalimat panjang 
+
+UserEmail = "FauziaAndriani8295@gmail.com";
+
+""" Kesimpulanya setiap nilai yang di tulis dalam tanda kutip satu maupun dua adalah string """
